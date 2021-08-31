7fd7a8c1f4079d0f3835cda0ab5197598e2bcbe2
TemmieGamerGuy <smith.nathaniel@gmail.com>
2021-08-31 17:20:42 +0100

    inversed ifs to make cleaner, removed useless stuff

--- AntiElevatorNade/AntiElevatorNade.cs
+++ AntiElevatorNade/AntiElevatorNade.cs
@@ -7,15 +7,10 @@ namespace AntiElevatorNade
     public class AntiElevatorNade : Plugin<Config>
     {
         private static AntiElevatorNade Singleton;
-
-        static AntiElevatorNade()
-        {
-        }
-
         public static AntiElevatorNade Instance => Singleton;
         public override string Author => "TemmieGamerGuy";
         public override string Name => "AntiElevatorNade";
-        public override Version Version => new Version(1, 0, 3);
+        public override Version Version => new Version(1, 0, 4);
         public override Version RequiredExiledVersion => new Version(3, 0, 0);
 
         private Handlers.Map map;
--- AntiElevatorNade/Handlers/Map.cs
+++ AntiElevatorNade/Handlers/Map.cs
@@ -8,33 +8,30 @@ namespace AntiElevatorNade.Handlers
         public void OnExplodingGrenade(ExplodingGrenadeEventArgs ev)
         {
             // Checks if grenade harms players
-            if (ev.IsFrag) 
-            {
-                // Checks if all affected players and thrower are of same side
-                if (ev.TargetsToAffect.Count >= AntiElevatorNade.Instance.Config.MinimumPlayersGrenade && ev.TargetsToAffect.All(player => ev.Thrower.Side == player.Side))
-                {
-                    foreach (Lift lift in Exiled.API.Features.Map.Lifts)
-                    {
-                        // Iterates through each elevator room
-                        foreach (Lift.Elevator elevator in lift.elevators)
-                        {
-                            // Checks if affected players are in the elevator
-                            if ((ev.TargetsToAffect.First().Position - elevator.target.position).sqrMagnitude < 13)
-                            {
-                                ev.IsAllowed = false; // Disables damage dealt to players
+            if (!ev.IsFrag) return;
 
-                                // Checks config to see if thrower should be damaged by their grenade and is affected by explosion
-                                if (AntiElevatorNade.Instance.Config.KillThrower == true && ev.TargetsToAffect.Contains(ev.Thrower))
-                                {
-                                    ev.Thrower.ShowHint(AntiElevatorNade.Instance.Config.ThrowerHint, 5); // Gives hint to thrower
-                                    ev.Thrower.Hurt(1000, ev.Thrower, DamageTypes.Grenade); // Kills the thrower
-                                }
-                                return;
-                            }
-                        }
-                    } 
+            // Checks if all affected players and thrower are of same side
+            if (!(ev.TargetsToAffect.Count != 0 && ev.TargetsToAffect.Count >= AntiElevatorNade.Instance.Config.MinimumPlayersGrenade && ev.TargetsToAffect.All(player => ev.Thrower.Side == player.Side))) return; 
+
+            foreach (Lift lift in Exiled.API.Features.Map.Lifts)
+            {
+                // Iterates through each elevator room
+                foreach (Lift.Elevator elevator in lift.elevators)
+                {
+                    // Checks if affected players are in the elevator
+                    if (!((ev.TargetsToAffect.First().Position - elevator.target.position).sqrMagnitude < 13)) continue; 
+
+                    ev.IsAllowed = false; // Disables damage dealt to players
+
+                    // Checks config to see if thrower should be damaged by their grenade and is affected by explosion
+                    if (AntiElevatorNade.Instance.Config.KillThrower == true && ev.TargetsToAffect.Contains(ev.Thrower))
+                    {
+                        ev.Thrower.ShowHint(AntiElevatorNade.Instance.Config.ThrowerHint, 5); // Gives hint to thrower
+                        ev.Thrower.Hurt(1000, ev.Thrower, DamageTypes.Grenade); // Kills the thrower
+                    }
+                    return;
                 }
-            }
+            } 
         }
     }
 }
