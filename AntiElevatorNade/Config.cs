35c9b8c98c2afc138edf934f78852fc3338efa59
Lorenzo <65295432+SpaceTm@users.noreply.github.com>
2021-08-31 15:02:51 +0200

    Now config and plugins r even cooler

--- AntiElevatorNade/Config.cs
+++ AntiElevatorNade/Config.cs
@@ -1,4 +1,4 @@
-﻿using Exiled.API.Interfaces;
+using Exiled.API.Interfaces;
 using System.ComponentModel;
 
 namespace AntiElevatorNade
@@ -6,7 +6,14 @@ namespace AntiElevatorNade
     public class Config : IConfig
     {
         public bool IsEnabled { get; set; } = true;
+
+        [Description("The minimum players to trigger the anti elevator grenade")]
+        public int MinimumPlayersGrenade { get; set; } = 2;
+
         [Description("Should the thrower of the grenade take damage from the explosion")]
         public bool KillThrower { get; set; } = true;
+
+        [Description("The message said to the thrower when they die, leave blank to disable")]
+        public string ThrowerHint { get; set; } = "Don't kill your teammates!";
     }
 }
