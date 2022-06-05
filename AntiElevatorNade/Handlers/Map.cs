83667df24c0ede132940314b8b951d60b50670df
xNexus <83370388+xNexus-ACS@users.noreply.github.com>
2022-06-05 03:19:07 +0200

    Updated to EXILED 5.2.1

--- AntiElevatorNade/Handlers/Map.cs
+++ AntiElevatorNade/Handlers/Map.cs
@@ -1,19 +1,20 @@
 using System.Linq;
+using Exiled.API.Enums;
 using Exiled.Events.EventArgs;
 
 namespace AntiElevatorNade.Handlers
 {
-    class Map
+    public class Map
     {
         public void OnExplodingGrenade(ExplodingGrenadeEventArgs ev)
         {
             // Checks if grenade harms players
-            if (!ev.IsFrag) return;
+            if (ev.GrenadeType != GrenadeType.FragGrenade) return;
 
             // Checks if all affected players and thrower are of same side
-            if (!(ev.TargetsToAffect.Count != 0 && ev.TargetsToAffect.Count >= AntiElevatorNade.Instance.Config.MinimumPlayersGrenade && ev.TargetsToAffect.All(player => ev.Thrower.Side == player.Side))) return; 
+            if (!(ev.TargetsToAffect.Count != 0 && ev.TargetsToAffect.Count >= AntiElevatorNade.Instance.Config.MinimumPlayersGrenade && ev.TargetsToAffect.All(player => ev.Thrower.Role.Side == player.Role.Side))) return; 
 
-            foreach (Lift lift in Exiled.API.Features.Map.Lifts)
+            foreach (Lift lift in Lift.Instances)
             {
                 // Iterates through each elevator room
                 foreach (Lift.Elevator elevator in lift.elevators)
@@ -27,7 +28,7 @@ namespace AntiElevatorNade.Handlers
                     if (AntiElevatorNade.Instance.Config.KillThrower && ev.TargetsToAffect.Contains(ev.Thrower))
                     {
                         ev.Thrower.ShowHint(AntiElevatorNade.Instance.Config.ThrowerHint, 5); // Gives hint to thrower
-                        ev.Thrower.Hurt(1000, ev.Thrower, DamageTypes.Grenade); // Kills the thrower
+                        ev.Thrower.Hurt(ev.Thrower, 1000f, DamageType.Explosion); // Kills the thrower
                     }
                     return;
                 }
